6bf4476e0e7f4d66e1eb1b48077696d731eea311
drwrchrds <drwrchrds@gmail.com>
2014-10-29 14:17:26 -0700

    still using jasmine-rails gem. tests run by starting server with "rails s -e test", but must restart server with each change to test suite

--- spec/javascripts/intro.spec.js
+++ spec/javascripts/intro.spec.js
@@ -1,13 +1,88 @@
+//= require pokedex.js
+
+describe("Pokedex.Models.Pokemon", function() {
+  it("has a urlRoot property", function() {
+    var poke = new Pokedex.Models.Pokemon();
+    expect(poke.urlRoot).toEqual('/pokemon');
+
+    poke.set('id', 1);
+    expect(poke.url()).toEqual('/pokemon/1');
+  });
+});
+
+describe("Pokedex.Collections.Pokemon", function() {
+  beforeEach(function() {
+    this.pokeCollection = new Pokedex.Collections.Pokemon();
+  });
+
+  it("knows its model", function() {
+    expect(this.pokeCollection.model).toEqual(Pokedex.Models.Pokemon);
+  });
+
+  it("knows its url", function() {
+    expect(this.pokeCollection.url).toEqual('/pokemon');
+  });
+});
+
 describe("listPokemon", function() {
+  var pokes = [
+    { 
+      name: "Charmander",
+      number: 4,
+      poke_type: "fire",
+      attack: 52,
+      defense: 43,
+      evolve_level: 16,
+      evolve_to: 5,
+      moves: ['scratch', 'ember', 'metal claw'],
+      levels: [5, 10],
+      curve: 1.3,
+      probability: 3
+    }, {
+      name: "Snorlax",
+      number: 143,
+      poke_type: 'normal'
+    }, {
+      name: 'Butterfree',
+      number: 12,
+      poke_type: 'bug'
+    }, {
+      name: 'Electabuzz',
+      number: 125,
+      poke_type: 'electric'
+    }, {
+      name: "Articuno",
+      number: 144,
+      poke_type: 'ice'
+    }
+  ];
+
+  beforeEach(function(done) {
+    var createdCount = 0;
+
+    pokes.forEach(function(poke) {
+      var model = new Pokedex.Models.Pokemon(poke)
+      model.save({}, {
+        success: function() {
+          createdCount += 1;
+          if(createdCount == 5) {
+            done();
+          }          
+        }
+      });
+    });
+  });
+
   it("fetches the passed-in collection", function() {
-    this.pokedex = new Pokedex();
-    this.pokes = new Pokedex.Collections.Pokemon();
+    var pokedex = new Pokedex();
+    var pokes = new Pokedex.Collections.Pokemon();
+
     var heardRequestTrigger = false;
-    this.pokes.on('request', function() {
+    pokes.on('request', function() {
       heardRequestTrigger = true;
     });
 
-    this.pokedex.listPokemon(this.pokes, function() {});
+    pokedex.listPokemon(pokes, function() {});
     expect(heardRequestTrigger).toEqual(true);
   });
 
@@ -28,7 +103,8 @@ describe("listPokemon", function() {
       expect(called).toEqual(true);
     });
 
-    it("waits to call the callback until the collection has been filled", function() {
+    it("waits to call the callback until the collection has been filled", 
+          function() {
       var pokeList = [];
       var that = this;
       that.pokes.each(function(poke) {
